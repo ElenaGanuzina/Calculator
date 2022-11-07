b6f2e11194e81432283dfc1b0781f2c0d0f4b2b3
Anton Vashenko <vashchenko-96@mail.ru>
2022-11-07 21:24:38 +0300

    Start telegram calc

--- interface.py
+++ interface.py
@@ -2,64 +2,82 @@ import exceptions as e
 import rational as r
 import complex as c
 import logger as l
+import main as m
+
+from telegram import ReplyKeyboardRemove
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+)
 
 
-def r_value_1():
-    return input('Введите первое число: ')
+def r_value_1(update, _):
+    update.message.reply_text('Введите первое число: ')
+    choice = update.message.text
+    return choice
 
 
-def r_value_2():
-    return input('Введите второе число: ')
+def r_value_2(update, _):
+    update.message.reply_text('Введите второе число: ')
+    choice = update.message.text
+    return choice
 
 
-def c_value_1():
-    valid = input('Введите действительную часть первого числа: ')
-    imaginary = input('Введите мнимую часть первого числа: ')
+def c_value_1(update, _):
+    update.message.reply_text('Введите действительную часть первого числа: ')
+    valid = update.message.text
+    update.message.reply_text('Введите мнимую часть первого числа: ')
+    imaginary = update.message.text
     return tuple([valid, imaginary])
 
 
-def c_value_2():
-    valid = input('Введите действительную часть второго числа: ')
-    imaginary = input('Введите мнимую часть второго числа: ')
+def c_value_2(update, _):
+    update.message.reply_text('Введите действительную часть второго числа: ')
+    valid = update.message.text
+    update.message.reply_text('Введите мнимую часть второго числа: ')
+    imaginary = update.message.text
     return tuple([valid, imaginary])
 
 
-def sqrt_value():
-    return input('Введите число: ')
+def sqrt_value(update, _):
+    update.message.reply_text('Введите число: ')
+    choice = update.message.text
+    return choice
 
 
-def for_rat(choice):
-    while True:
-        if choice == "8":
-            num = sqrt_value()
-            if e.check_num(num):
-                r.init(eval(num), 0)
-                res = r.do_it(choice)
-                print(f"Результат: {res}")
-                l.log(num, 0, res, choice)
-                exit()
-            else:
-                print("Некорректный ввод\n")
+def for_rat(choice, update, _):
+    if choice == "8":
+        num = sqrt_value(update, _)
+        if e.check_num(num):
+            r.init(eval(num), 0)
+            res = r.do_it(choice)
+            update.message.reply_text(f"Результат: {res}")
+            l.log(num, 0, res, choice)
+            m.cancel(update, _)
         else:
-            num_1 = r_value_1()
-            num_2 = r_value_2()
-            if e.check_num(num_1) and e.check_num(num_2):
-                if choice in ['4', '5', '6'] and num_2 == '0':
-                    print("На 0 делить нельзя\n")
-                else:
-                    num_1, num_2 = eval(num_1), eval(num_2)
-                    r.init(num_1, num_2)
-                    res = r.do_it(choice)
-                    print(f"Результат: {res}\n")
-                    l.log(num_1, num_2, res, choice)
-                    exit()
+            update.message.reply_text("Некорректный ввод\n")
+    else:
+        num_1 = r_value_1(update, _)
+        num_2 = r_value_2(update, _)
+        if e.check_num(num_1) and e.check_num(num_2):
+            if choice in ['4', '5', '6'] and num_2 == '0':
+                update.message.reply_text("На 0 делить нельзя\n")
             else:
-                print("Некорректный ввод\n")
+                num_1, num_2 = eval(num_1), eval(num_2)
+                r.init(num_1, num_2)
+                res = r.do_it(choice)
+                update.message.reply_text(f"Результат: {res}\n")
+                l.log(num_1, num_2, res, choice)
+                m.cancel(update, _)
+        else:
+            update.message.reply_text("Некорректный ввод\n")
 
 
-def menu_rat():
-    while True:
-        txt = """Введите
+def menu_rat(update, _):
+    txt = """Введите
         для сложения: 1
         для вычитания: 2
         для умножения: 3
@@ -70,68 +88,63 @@ def menu_rat():
         для извлечения из квадратного корня: 8
         для выхода: q
         для возврата в предыдущее меню: r"""
-        print(txt)
-        choice = input("Ввод: ")
-        if e.check_rat(choice):
-            for_rat(choice)
-        elif e.check_back(choice):
-            return
-        elif e.check_exit(choice):
-            exit()
-        else:
-            print("Некорректный ввод\n")
+    update.message.reply_text(txt)
+    update.message.reply_text("Ввод: ")
+    choice = update.message.text
+    if e.check_rat(choice):
+        for_rat(choice, update, _)
+    elif e.check_back(choice):
+        return
+    elif e.check_exit(choice):
+        m.cancel(update, _)
+    else:
+        update.message.reply_text("Некорректный ввод\n")
 
 
-def for_comp(choice):
-    while True:
-        num_1 = c_value_1()
-        num_2 = c_value_2()
-        if e.check_num(num_1[0]) and e.check_num(num_1[1]) and \
-                e.check_num(num_2[0]) and e.check_num(num_2[1]):
-            c.init_x(eval(num_1[0]), eval(num_1[1]))
-            c.init_y(eval(num_2[0]), eval(num_2[1]))
-            res = c.do_it(choice)
-            print(f"Результат: {res}\n")
-            l.log(c.x, c.y, res, choice)
-            exit()
-        else:
-            print("Некорректный ввод\n")
+def for_comp(choice, update, _):
+    num_1 = c_value_1(update, _)
+    num_2 = c_value_2(update, _)
+    if e.check_num(num_1[0]) and e.check_num(num_1[1]) and \
+            e.check_num(num_2[0]) and e.check_num(num_2[1]):
+        c.init_x(eval(num_1[0]), eval(num_1[1]))
+        c.init_y(eval(num_2[0]), eval(num_2[1]))
+        res = c.do_it(choice)
+        update.message.reply_text(f"Результат: {res}\n")
+        l.log(c.x, c.y, res, choice)
+        m.cancel(update, _)
+    else:
+        update.message.reply_text("Некорректный ввод\n")
 
 
-def menu_comp():
-    while True:
-        txt = """Введите
+def menu_comp(update, _):
+    txt = """Введите
         для сложения: 1
         для вычитания: 2
         для умножения: 3
         для обычного деления: 4
         для выхода: q
         для возврата в предыдущее меню: r"""
-        print(txt)
-        choice = input("Ввод: ")
-        if e.check_comp(choice):
-            for_comp(choice)
-        elif e.check_back(choice):
-            return
-        elif e.check_exit(choice):
-            exit()
-        else:
-            print("Некорректный ввод\n")
+    update.message.reply_text(txt)
+    update.message.reply_text("Ввод: ")
+    choice = update.message.text
+    if e.check_comp(choice):
+        for_comp(choice, update, _)
+    elif e.check_back(choice):
+        return
+    elif e.check_exit(choice):
+        m.cancel(update, _)
+    else:
+        update.message.reply_text("Некорректный ввод\n")
 
 
-def choice_num():
-    while True:
-        txt = '''Введите
-        для работы с рациональными числами: 1
-        для работы с комплексными числами: 2
-        для выхода: q'''
-        print(txt)
-        choice = input("Ввод: ")
-        if choice == '1':
-            menu_rat()
-        elif choice == '2':
-            menu_comp()
-        elif e.check_exit(choice):
-            exit()
-        else:
-            print("Некорректный ввод\n")
+def choice_num(update, _):
+    choice = update.message.text
+    if choice == '1':
+        return m.CHOICE_2
+    elif choice == '2':
+        return m.CHOICE_3
+    elif e.check_exit(choice):
+        m.cancel(update, _)
+    else:
+        update.message.reply_text("Некорректный ввод")
+        m.start(update, _)
--- main.py
+++ main.py
@@ -1,3 +1,66 @@
 import interface as i
 
-i.choice_num()
+from telegram import ReplyKeyboardRemove
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+)
+
+# Определяем константы этапов разговора
+# fld = list(range(1, 10))
+# x = chr(10060)
+# o = chr(11093)
+# count = 9
+# player = x
+CHOICE_1 = 0
+CHOICE_2 = 1
+CHOICE_3 = 2
+CHOICE_4 = 3
+
+
+# функция обратного вызова точки входа в разговор
+def start(update, _):
+    txt = '''Введите
+            для работы с рациональными числами: 1
+            для работы с комплексными числами: 2
+            для выхода: q'''
+    update.message.reply_text(txt)
+    update.message.reply_text("Ввод: ")
+    return CHOICE_1
+
+
+def choice(update, _):
+    # move = update.message.text
+    # move = int(move)
+    # update.message.reply_text(f"Некорректный ввод{chr(9940)}\nПопробуйте еще раз")
+    i.choice_num(update, _)
+
+
+def cancel(update, _):
+    update.message.reply_text('Пока', reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+if __name__ == '__main__':
+    updater = Updater("TOKEN")
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            CHOICE_1: [MessageHandler(Filters.text & ~Filters.command, choice)],
+            CHOICE_2: [MessageHandler(Filters.text & ~Filters.command, i.menu_rat)],
+            CHOICE_3: [MessageHandler(Filters.text & ~Filters.command, i.menu_comp)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    print('server start')
+
+    updater.start_polling()
+    updater.idle()
